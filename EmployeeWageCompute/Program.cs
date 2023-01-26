baf5c09436f3a6e6ea03b551db4b0ab5d3d80dab
preeti <pritich0896@gmail.com>
2023-01-27 01:35:44 +0530

    [Preeti]Modified calculating employee attedence

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace EmployeeWageCompute
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome to EmployeeWage Computation");
+
+            //UC1 -EmployeeAttendence
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);//0 or 1
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("employee is Absent");
+            }
             Console.ReadLine();
         }
     }
