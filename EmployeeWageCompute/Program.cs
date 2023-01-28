d4198b51fb783f64bc369bad1377f469ab05a04c
preeti <pritich0896@gmail.com>
2023-01-29 00:26:53 +0530

    [Preeti]Modified PartTime EmpWage

--- EmployeeWageCompute/Program.cs
+++ EmployeeWageCompute/Program.cs
@@ -10,18 +10,24 @@ namespace EmployeeWageCompute
         static void Main(string[] args)
         {
             const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
                 
             Console.WriteLine("Welcome to EmployeeWage Computation");
 
-            //UC2 -EmployeeAttendence
+            //UC3 -EmployeeAttendence
             Random random = new Random();
-            int empAttendence = random.Next(0, 2);//0 or 1
+            int empAttendence = random.Next(0, 3);//0 or 1 or 2
             if (empAttendence == FULL_TIME)
             {
-                Console.WriteLine("employee is Present");
+                Console.WriteLine("Full Time employee is Present");
                 empHrs = 8;
+            }
+            else if(empAttendence == PART_TIME)
+            {
+                Console.WriteLine("Part Time employee is present");
+                empHrs = 4;
             }
             else
             {
